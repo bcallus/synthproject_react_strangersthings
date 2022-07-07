47cc7826af3381aebea7e7399dbf90c539b21416
Brittany <brittanylcallus@gmail.com>
2022-07-07 19:27:57 -0400

    test file changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// will need to import each component
+/*import {
+  Component1,
+  Component2,
+  etc,
+  etc,
+} from "./components";*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div>Test</div>
+  )
+
+};
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
