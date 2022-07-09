c65ee21d0f559f9a95301af4e07555ec4c32d1bf
Brittany <brittanylcallus@gmail.com>
2022-07-09 13:23:57 -0400

    Post component added

---
+++ src/components/Post.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Post = (props) => {
+
+    return (
+
+        <div className="individual-post" key={props.post._id}>
+                <h2>{props.post.title}</h2>
+                <p>{props.post.description}</p>
+                <p>{props.post.price}</p>
+                <h2>{props.post.author.username}</h2>
+                <p>{props.post.locaiton}</p>
+        </div>
+    )
+
+}
+
+export default Post;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,38 +1,23 @@
 import React from 'react'
+import Post from './Post'
 
 
 const Posts = (props) => {
     
     console.log("postsList", props.postsList)
     console.log("props.postsList.posts", props.postsList.posts)
-    // props.postsList.posts ? props.postsList.posts.map(post => {
-    //     console.log("title", post.title)
-    //     return (
-    //         <div>
-    //             <h2>post.title</h2>
-    //             <p>post.description</p>
-    //             <p>post.price</p>
-    //             <h2>post.author.username</h2>
-    //             <p>post.locaiton</p>
-    //         </div>
-    //     )
-    // }
-    // ) : console.log("null");
-    // console.log("post", props.post)
+
     return (
         <div>
         {props.postsList.posts ? props.postsList.posts.map(post => 
-                <div key={post._id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.description}</p>
-                    <p>{post.price}</p>
-                    <h2>{post.author.username}</h2>
-                    <p>{post.locaiton}</p>
-                </div>
-        ) : console.log("null")};
+            <Post 
+            post={post}
+            />
+        ) : null}
         </div>
     )
 
 }
 
 export default Posts;
+
