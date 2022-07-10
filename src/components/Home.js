221930bc7f056c9a0c71e96801bfa9b9ed1aa1f2
Brittany <brittanylcallus@gmail.com>
2022-07-10 12:07:31 -0400

    home page starter text

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <h2>HomeTest</h2>
+        <div>
+            <h1>Welcome to Stranger's Things!</h1>
+            <h2>View public posts by clicking Post tab.</h2>
+            <h2>Log in to view your profile and get access to posts.</h2>
+            <h2>New to Stranger's Things? Click Here to Register a new account.</h2>
+        </div>
     )
 }
 
