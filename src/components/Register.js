b9c3921d36d43f52b572768998a400af7b8051ed
Brittany <brittanylcallus@gmail.com>
2022-07-10 12:29:19 -0400

    basic register form created

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <h2>RegisterTest</h2>
+        <form>
+            <h2>Register New Account</h2>
+            <label>
+                <p>Username</p>
+                <input />
+            </label>
+            <label>
+                <p>Password</p>
+                <input />
+            </label>
+            <div>
+                <button type="submit" >Submit</button>
+            </div>
+        </form>
     )
 }
 
