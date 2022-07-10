2d29a19bc733e14fa6b518c218537114266f5880
Brittany <brittanylcallus@gmail.com>
2022-07-10 10:57:57 -0400

    moved App component into its own file

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+import { fetchAllPosts } from './api'
+
+// will need to import each component
+import {
+  Navbar,
+  Home,
+  Posts,
+  Profile,
+  //etc,
+  //etc,
+} from "./components";
+
+const App = () => {
+
+  const [postsList, setPostsList] = useState([]);
+  // const [post, setPost] = useState({});
+
+
+    useEffect(() => {
+        fetchAllPosts().then((results) => {
+            // console.log("post", results.data.posts)
+          setPostsList(results.data)
+        })
+    }, []);
+    
+  // console.log("postsList", postsList)
+  // console.log("post", post)
+
+  return (
+    <div> 
+      <Navbar />
+      <Home />
+      <Posts 
+        postsList={postsList}
+        // post={post}
+        // setPost={setPost}
+      />
+      <Profile />
+    </div>
+  )
+
+};
+
+export default App;
