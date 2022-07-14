575e50317afe34294be5a3cdfa5fa266f2889996
Brittany <brittanylcallus@gmail.com>
2022-07-13 23:47:10 -0400

    savepoint

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -3,28 +3,30 @@ import React from "react";
 const AddPost = () => {
   return (
     <form>
-          <h1>Add New Post</h1>
-          <label>
-              <p>Title</p>
-              <input />
-          </label>
-          <label>
-              <p>Description</p>
-              <input />
-          </label>
-          <label>
-              <p>Price</p>
-              <input />
-          </label>
-          <label>
-              <p>Location</p>
-              <input />
-          </label>
-          <label>
-              <p>Willing to deliver</p>
-              <input type="checkbox" />
-          </label>
-          <button type="submit">Create</button>
+      <h1>Add New Post</h1>
+      <label>
+        <p>Title</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Description</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Price</p>
+        <input /> {/*make type for currency? */}
+      </label>
+      <label>
+        <p>Location</p>
+        <input type="text" />
+      </label>
+      <label>
+        <div>
+          <p>Willing to deliver</p>
+          <input type="checkbox" /> {/*fix inline style here */}
+        </div>
+      </label>
+      <button type="submit">Create</button>
     </form>
   );
 };
