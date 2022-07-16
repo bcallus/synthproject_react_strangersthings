8b03096467cb8d313c67f5c027ad7cf19da93006
Brittany <brittanylcallus@gmail.com>
2022-07-16 18:36:50 -0400

    messages user sent rendered to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,10 +6,10 @@ const Profile = ({ token, username, userPosts, userMessages, setUserPosts, setUs
     async function fetchMyProfile() {
       let data = await getUserProfile({ token });
         console.log("getUserProfile data", data)
-        setUserPosts(data.data.posts)
+        // setUserPosts(data.data.posts)
         console.log("user posts from Profile.js", userPosts);
         setUserMessages(data.data.messages)
-        console.log("user messages from Profile.js", userMessages);
+        // console.log("user messages from Profile.js", data.data.messages.map(message => console.log(message.content)));
     }
     fetchMyProfile();
   }, []); //hover over squiggle. what does it mean?
@@ -17,9 +17,17 @@ const Profile = ({ token, username, userPosts, userMessages, setUserPosts, setUs
     //map over posts and messages once there are some in the data
   return (
     <div>
-          <h2>Welcome {username}!</h2>
-          <h2>Your Posts</h2>
-          <h2>Your Messages</h2>
+      <h2>Welcome {username}!</h2>
+      <h2>Your Posts</h2>
+      <h2>Messages Sent By Me</h2>
+      {userMessages.map(message => {
+        return (
+          <div>
+            {message.content} 
+            </div>
+        )
+      })}
+      <h2>Your Messages</h2>
     </div>
   );
 };
