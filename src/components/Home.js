3de3e1798e45aa9b1f4963e2fc62604a8dc33c22
Brittany <brittanylcallus@gmail.com>
2022-07-17 16:16:10 -0400

    home page css added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className="home-page">
-            <h1>Welcome to Stranger's Things!</h1>
-            <h2>View public posts by clicking Post tab.</h2>
-            <h2>Log in to view your profile and get access to posts.</h2>
-            <h2>New to Stranger's Things? <Link to="/register">Click Here</Link> to Register a new account.</h2>
+        <div>
+            <div className="home-title">
+                <h1>Welcome to Stranger's Things</h1>
+                <p>The place to buy and sell your things!</p>
+            </div>
+            <div className="home-body">
+            <h2>Here are a few helpful tips to help you get started.</h2>
+            <h3>Want to take a look at what people are selling?</h3>
+            <p>Visit the Posts tab to browse through public postings.</p>
+            <h3>See something you like?</h3>
+            <p>Log in to view your profile and unlock access to posts.</p>
+            <h3>New to Stranger's Things?</h3>
+            <p><Link to="/register">Click Here</Link> to register a new account.</p>
+            </div>
         </div>
     )
 }
