635a6113a76287e67ef4f0fbc14edcc56e06a987
Brittany <brittanylcallus@gmail.com>
2022-07-16 23:03:02 -0400

    messages sent to me console logged but not rendered.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,18 +7,24 @@ const Profile = ({ token, username, userPosts, userMessages, setUserPosts, setUs
       let data = await getUserProfile({ token });
         console.log("getUserProfile data", data)
         setUserPosts(data.data.posts)
-        console.log("user posts from Profile.js", userPosts);
+        console.log("userPosts from Profile.js", userPosts);
       setUserMessages(data.data.messages)
-      console.log("inbox messages from Profile.js", data.data.posts)
-      data.data.posts.map(post => {
-        return (
-          setInboxMessages(post.messages)
-        )
-      })
-
     }
     fetchMyProfile();
   }, []); //hover over squiggle. what does it mean?
+
+  console.log("inbox messages from Profile.js", inboxMessages)
+
+  userPosts.map(userPost => {
+    if (userPost.messages.length > 0) {
+      console.log("userPost.messages", userPost.messages)
+      const inboxMessages = userPost.messages
+      console.log("inboxMessages", inboxMessages)
+      inboxMessages.map(message => {
+      console.log(message.content)
+      })
+    }
+  })
 
     //map over posts and messages once there are some in the data
   return (
@@ -35,14 +41,22 @@ const Profile = ({ token, username, userPosts, userMessages, setUserPosts, setUs
         )
       })}
       <h2>Messages Sent To Me</h2>
-      {inboxMessages.map(message => {
+      {userPosts.map(userPost => {
+    if (userPost.messages.length > 0) {
+      console.log("userPost.messages", userPost.messages)
+      const inboxMessages = userPost.messages
+      console.log("inboxMessages", inboxMessages)
+      inboxMessages.map(message => {
         return (
-          <div className="messages" key={message._id}>
-            {/* <h3>Post Title: {userPosts.title}</h3> */}
-            <p><b>Message Sent: </b>{message.content}</p>
+          <div>
+            {console.log(message.content)}
+            <h2>{message.content}</h2>
           </div>
         )
+      })
+    }
       })}
+      <h2>test</h2>
     </div>
   );
 };
