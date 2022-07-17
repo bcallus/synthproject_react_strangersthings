c3db7c8f6d806989e6594e74c633a0164633e7b3
Brittany <brittanylcallus@gmail.com>
2022-07-17 11:12:43 -0400

    search bar keyword properly logs filtered results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SearchBar = (postsList) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [searchTerm, setSearchTerm] = useState("")
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    console.log(postsList.postsList);
+    // console.log("searchTerm", searchTerm);
 
-    const handleSearch = async (event) => {
-        event.preventDefault();
-        console.log(postsList)
+    function findMatches(post, keyword) {
+        keyword = searchTerm.toLowerCase();
         console.log("searchTerm", searchTerm)
-
-        function findMatches (postsList, searchTerm) {
-            
-        }
+          console.log("keyword", keyword)
+      if (
+        post.title.toLowerCase().includes(keyword) ||
+        post.description.toLowerCase().includes(keyword) ||
+        post.price.includes(keyword) ||
+        post.location.toLowerCase().includes(keyword)
+      )
+        return true;
     }
+      
+    const filteredPosts = postsList.postsList.filter(post => findMatches(post, searchTerm));
+    console.log("filteredPosts", filteredPosts)
+    // const postsToDisplay = searchTerm.length ? filteredPosts : posts;
+  };
 
-    return (
-        <form onSubmit={handleSearch}>
-            <input type="text" placeholder="Search..." onChange={(event) => setSearchTerm(event.target.value)}/>
-            <button type="submit">Submit</button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default SearchBar;
