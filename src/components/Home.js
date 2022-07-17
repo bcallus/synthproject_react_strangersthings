709a2a403cbf2de24010f52897d878e57d20f903
Brittany <brittanylcallus@gmail.com>
2022-07-17 14:21:30 -0400

    home page css savepoint

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-page">
             <h1>Welcome to Stranger's Things!</h1>
             <h2>View public posts by clicking Post tab.</h2>
             <h2>Log in to view your profile and get access to posts.</h2>
